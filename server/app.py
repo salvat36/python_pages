6d9187e987c382118b63f0f15b4bc56031940307
Ryan <salvat36@gmail.com>
2023-06-23 03:54:50 -0500

    make login/signup non-restful routes in app.py

--- server/app.py
+++ server/app.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 
 # Standard library imports
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-# from config import app, db, api
 from models import User, UserBook, Book, db
 import os
 from flask_restful import Resource, Api
 
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get(
-    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, '/instance/app.db')}")
+DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, '/instance/app.db')}")
 
 app = Flask(__name__)
 
@@ -35,8 +33,40 @@ db.init_app(app)
 
 
 @app.route('/')
-def index():
+def home():
     return '<h1> home page</h1>'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    user = User.query.filter(User.username == username).first()
+
+    if user:
+        ##!Need password authentication here
+        return (user.to_dict(), 200)
+        
+    
+    return {"Error": "404 Unauthorized"}, 401
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    data = request.get_json()
+    user = User(**data)
+    try:
+        db.session.add(user)
+        db.session.commit()
+        session['user_id'] = user.id
+        return make_response(user.to_dict(), 201)
+    except Exception as e:
+        return make_response({"Error": "str(e)"}, 422)
+
+
+
+
+
 
 
 class UserBooks(Resource):
@@ -109,22 +139,6 @@ class UserById(Resource):
 
 api.add_resource(UserById, '/users/<int:id>')
 
-
-class Login(Resource):
-    def post(self):
-        data = request.get_json()
-        username = data.get('username')
-        password = data.get('password')
-    
-        user = User.query.filter(User.username == username).first()
-
-        if user:
-            return (user.to_dict(), 200)
-            ##!Need password here too
-        
-        return {"Error": "404 Unauthorized"}, 401
-api.add_resource(Login, '/login>')
-        
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
