aab50bd7a1b2f18d13b7baf79e4b54d451c8d1a7
Ryan <salvat36@gmail.com>
2023-06-24 19:45:51 -0500

    display UserName/password requirment message

--- client/src/components/Authentication.js
+++ client/src/components/Authentication.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
-import { Switch, Route, useParams, useHistory } from "react-router-dom";
-import { useFormik } from "formik";
-import * as yup from "yup";
+import { Switch, Route, useParams, useHistory } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import { Form, Button, Message } from 'semantic-ui-react';
 
 const Authentication = ( { updateUser } ) => {
@@ -14,26 +14,27 @@ const Authentication = ( { updateUser } ) => {
     const formSchema = yup.object().shape({
         username: yup
           .string()
-          .required("Username Required")
-          .min(5, "Too Short!")
-          .max(80, "Too Long!"),
-        password: yup.string()
-          .required("Password Required")
-          .min(5)
-          .max(80),
+          .required('Username Required')
+          .min(5, 'Must be at least 5 characters')
+          .max(80, 'Cannot go over 80 characters'),
+        password: yup
+          .string()
+          .required('Password Required')
+          .min(5, 'Must be at least 5 characters')
+          .max(80, 'Cannot go over 80 characters')
       });
     
       const formik = useFormik({
         initialValues: {
-          username: "",
-          password: "",
+          username: '',
+          password: '',
         },
         validationSchema: formSchema,
         onSubmit: (values, { resetForm }) => {
           fetch(signUp? '/signup' : '/login', {
-            method: "POST",
+            method: 'POST',
             headers: {
-              "Content-Type": "application/json",
+              'Content-Type': 'application/json',
             },
             body: JSON.stringify(values),
           })
@@ -41,7 +42,7 @@ const Authentication = ( { updateUser } ) => {
               if (res.ok) {
                 res.json().then((res) => {
                     updateUser(res)
-                    resetForm()
+                    resetForm( {values: formik.initialValues} )
                     history.push('/')
                 }
                 )} else {
@@ -57,10 +58,10 @@ const Authentication = ( { updateUser } ) => {
           <h2>{signUp? 'Create an Account Below: ' : 'Login Below: '} </h2>
           <Form onSubmit={formik.handleSubmit}>
             <Form.Field>
-            <label htmlFor="username">Username: </label>
+            <label htmlFor='username'>Username: </label>
             <input
-              type="text"
-              name="username"
+              type='text'
+              name='username'
               value={formik.values.username}
               onChange={formik.handleChange}
             />
@@ -69,19 +70,19 @@ const Authentication = ( { updateUser } ) => {
             ) : null}
             </Form.Field>
             <Form.Field>
-            <label htmlFor="password">Password: </label>
+            <label htmlFor='password'>Password: </label>
             <input
-              type="password"
-              name="password"
+              type='password'
+              name='password'
               value={formik.values.password}
               onChange={formik.handleChange}
             />
             {formik.errors.password && formik.touched.password ? (
-              <Message error content ={formik.errors.username}/>
+              <Message error content ={formik.errors.password}/>
             ) : null}
             </Form.Field>
     
-            <Button type="submit">Login</Button>
+            <Button type='submit'>Login</Button>
           </Form>
           <Button onClick={handleClick}>{signUp? 'Login here!' :  'Create an Account!'}</Button>
         </div>
