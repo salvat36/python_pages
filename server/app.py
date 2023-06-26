c5d5c2cc3ab8c88c06b2cb3fe410b056232c24eb
Cody Cogbill <codeman24@gmail.com>
2023-06-26 17:55:39 -0600

    working on routes
    added some routes

--- server/app.py
+++ server/app.py
@@ -26,12 +26,54 @@ db.init_app(app)
 
 # Views go here!
 
+
+@app.route('/')
+def home():
+    return ''
+
+
 class UserBooks(Resource):
     def get(self):
         user_books = [ub.to_dict() for ub in UserBook.query.all()]
         return make_response(user_books, 200)
 
+
+# need post for adding new user_books
 api.add_resource(UserBooks, '/user-books')
+
+
+class UserBooksById(Resource):  # ! delete goes here
+    def get(self, id):
+        user = UserBooks.query.filter(UserBooks.id == id).first()
+        if user:
+            return user
+        make_response({'error': 'Library not found'})
+
+    def delete(self, id):
+        try:
+            user_book = db.session.get(UserBook, id)
+            db.session.delete(user_book)
+            db.session.commit()
+            return make_response('', 200)
+        except:
+            return make_response({'error(s):' 'delete not successful'})
+
+
+api.add_resource(UserBooks, '/user-books')
+
+
+class Book(Resource):
+    def get(self):
+        books = [b.to_dict() for b in Book.query.all()]
+        return make_response(books, 200)
+
+
+api.add_resource(Book, '/books')
+
+
+class User(Resource): ##gets a post 
+    pass
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
