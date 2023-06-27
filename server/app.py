9c41a6b966e24c6152f5a9972e6dcac2ded6023a
Cody Cogbill <codeman24@gmail.com>
2023-06-27 09:29:44 -0600

    added more routing and fixed up some things
    done

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Standard library imports
-from flask import Flask, request, jsonify, make_response, Request
+from flask import Flask, request, jsonify, make_response
 from flask_migrate import Migrate
 from flask_restful import Resource, Api
 from config import app, db, api
@@ -9,8 +9,7 @@ from models import User, UserBook, Book
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get(
-    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'instance/app.db')}")
+DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'instance/app.db')}")
 
 app = Flask(__name__)
 
@@ -38,28 +37,21 @@ class UserBooks(Resource):
         return make_response(user_books, 200)
 
 
-# need post for adding new user_books
-api.add_resource(UserBooks, '/user-books')
-
-
-class UserBooksById(Resource):  # ! delete goes here
+class UserBooksById(Resource):
     def get(self, id):
-        user = UserBooks.query.filter(UserBooks.id == id).first()
+        user = UserBook.query.filter(UserBook.id == id).first()
         if user:
-            return user
-        make_response({'error': 'Library not found'})
+            return user, 200
+        return make_response({'error': 'User not found'}, 404)
 
     def delete(self, id):
         try:
-            user_book = db.session.get(UserBook, id)
+            user_book = UserBook.query.get(id)
             db.session.delete(user_book)
             db.session.commit()
             return make_response('', 200)
-        except:
-            return make_response({'error(s):' 'delete not successful'})
-
-
-api.add_resource(UserBooks, '/user-books')
+        except Exception as e:
+            return make_response({'error': str(e)}, 500)
 
 
 class Book(Resource):
@@ -68,11 +60,35 @@ class Book(Resource):
         return make_response(books, 200)
 
 
+class User(Resource):
+    def get(self):
+        users = [u.to_dict() for u in User.query.all()]
+        return make_response(users, 200)
+
+    def post(self):
+        try:
+            new_user = User(username=request.json['username'], password=request.json['password'])
+            db.session.add(new_user)
+            db.session.commit()
+            return new_user.to_dict(only=('username', 'password')), 201
+
+        except Exception:
+            return make_response({'error': '400: Validation error'}, 400)
+
+
+class UserById(Resource):
+    def get(self, id):
+        user = User.query.filter(User.id == id).first()
+        if user:
+            return user, 200
+        return make_response({'error': 'User not found'}, 404)
+
+
+api.add_resource(UserBooks, '/user-books')
+api.add_resource(UserBooksById, '/user-books/<int:id>')
 api.add_resource(Book, '/books')
-
-
-class User(Resource): ##gets a post 
-    pass
+api.add_resource(User, '/users')
+api.add_resource(UserById, '/users/<int:id>')
 
 
 if __name__ == '__main__':
